3d08152d2c9eced51c85f3a0950dec3f71b36d1e
victor1123-alt <victoriaamara979@gmail.com>
2023-10-10 07:01:00 +0100

    slider changes

--- output/javascript/index.js
+++ output/javascript/index.js
@@ -5,73 +5,159 @@ let featuredBooksTemplate = document.querySelector('[data-featured-books-templat
 let featuredBooksContainer = document.querySelector('[data-feauture-books-container]');
 
 // slider
-let slider = document.querySelectorAll('[data-slider]')
-let counter = 0;
+let Islider = document.querySelectorAll('[data-slider]');
+let sliderNextButton = document.querySelector('[data-slider-next]');
+let sliderPreviousButton = document.querySelector('[data-slider-previous]');
 
-function sliderFunction() {
-
-   for (let i = 0; i < slider.length; i++) {
-    const element = slider[i];
-    
-    if (i == counter) {
-        element.classList.add('currentslide');
-        element.classList.remove('offslide')
-        // console.log({counter,i,element});
-
-    }else{
-        element.classList.remove('currentslide')
-        element.classList.add('offslide')
+class Slider {
+    constructor(slider,sliderIndex) {
+        this.slider = slider
+        this.counter = 0
+        this.sliderIndex = sliderIndex
+        // this.updateSlide()
     }
-   }  
-    counter = counter+1
+
+    nextSlide() {
+        
+        console.log(this.counter);
+        if (this.counter >= this.slider.length - 1) {
+            this.counter = 0
+        } else {
+            this.counter = this.counter + 1
+        }
+        this.updateSlide()
+    }
+
+    quickSwitch(toX){
+
+        this.counter= toX;
+        this.updateSlide()
+
+    }
+
+    prevSlide() {
+        if (this.counter <= 0) {
+            this.counter = this.slider.length -1
+        } else {
+            this.counter = --this.counter 
+        }
+        this.updateSlide()
+
+        console.log({PEV:this.counter});
+
+    }
+
+    autoSlide() {
+        
+        if (this.counter >= this.slider.length - 1) {
+            this.counter = 0
+        } else {
+            this.counter = this.counter + 1
+        }
+
+        this.updateSlide()
+        console.log(this.counter);
+    }
+
+
+    updateSlide() {
+        
+        for (let i = 0; i < this.slider.length; i++) {
+            const element = this.slider[i];
+
+            // console.log(element);
+            if (i == this.counter) {
+                element.classList.add('currentslide');
+                element.classList.remove('offslide')
+
+                // console.log({ counter, i, element });
+
+            } else {
+                element.classList.remove('currentslide')
+                element.classList.add('offslide')
+
+            }
+
+            if (sliderIndex[i].dataset.slider_index == this.counter) {
+                sliderIndex[i].classList.add('bg-gray-600')
+
+            }else{
+                sliderIndex[i].classList.remove('bg-gray-600')
+
+            }
+        }
+    }
 }
 
-setInterval(sliderFunction, 1000)
+let sliderIndex = document.querySelectorAll('[data-pointer]')
+
+
+
+const ImageSlider  = new Slider(Islider,sliderIndex)
+setInterval(()=>{let automaticSlide = ImageSlider.autoSlide()
+}, 8000)
+
+sliderIndex.forEach(index=>{
+    // ImageSlider.quickSwitch(index)
+    index.onclick = ()=>{
+    ImageSlider.quickSwitch(Number(index.dataset.slider_index))
+
+    }
+})
+
+sliderNextButton.onclick = ()=>{
+    ImageSlider.nextSlide()
+}
+
+sliderPreviousButton.onclick = ()=>{
+    ImageSlider.prevSlide()
+}
+
 
 //******************************** Book fetch section ****************************************************/
 // let apiLink = "https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=8b6TdVsJ9fXIxnCshkqxHGfUWCWmCTQa"
 let apiLink = /*"https://www.dbooks.org/api/recent" */"../books.json"
-FetchBooks(apiLink,reslovedFunction,rejectedFunction)
+FetchBooks(apiLink, reslovedFunction, rejectedFunction)
 let id
-const viewsingleBooks = (e)=>{
+const viewsingleBooks = (e) => {
     id = e
 
-    document.location.href = "./singleBooks.html?id="+id
+    document.location.href = "./singleBooks.html?id=" + id
 }
 
 
-function reslovedFunction (x){
-   let books = x.books;
-   shuffleArray(books)
-   
-   books.map((singleBook,key)=>{
-    let singleBooks = featuredBooksTemplate.content.cloneNode(true).children[0]
-    let head = singleBooks.querySelector('[data-head]');
-    let body = singleBooks.querySelector('[data-body]')
-    let link = singleBooks.querySelector('[data-link]');
-    let dataImage = singleBooks.querySelector('[data-image]')
-    let author = singleBooks.querySelector('[data-author]');
+function reslovedFunction(x) {
+    let books = x.books;
+    shuffleArray(books)
 
-    author.textContent = singleBook.authors
+    books.map((singleBook, key) => {
+        let singleBooks = featuredBooksTemplate.content.cloneNode(true).children[0]
+        let head = singleBooks.querySelector('[data-head]');
+        let body = singleBooks.querySelector('[data-body]')
+        let link = singleBooks.querySelector('[data-link]');
+        let dataImage = singleBooks.querySelector('[data-image]')
+        let author = singleBooks.querySelector('[data-author]');
 
-    singleBooks.id = singleBook.id;
-    singleBooks.style.zIndex = 10000
-    singleBooks.onclick = ()=>{viewsingleBooks(singleBook.id)}
-    dataImage.src = singleBook.image
-    link.href = singleBook.url
+        author.textContent = singleBook.authors
 
-    
+        singleBooks.id = singleBook.id;
+        singleBooks.style.zIndex = 10000
+        singleBooks.onclick = () => { viewsingleBooks(singleBook.id) }
+        dataImage.src = singleBook.image
+        link.href = singleBook.url
 
-    if (singleBook.title !=="" && singleBook.summary !=="" && key< 12 && key>=0) {
-        featuredBooks.append(singleBooks)
-    }else{
-        console.log(key);
-    }
 
-   
-   })
 
-//    console.log(books);
+        if (singleBook.title !== "" && singleBook.summary !== "" && key < 12 && key >= 0) {
+            featuredBooks.append(singleBooks)
+        } else {
+            console.log(key);
+        }
+
+
+    })
+
+    //    console.log(books);
 }
 
 function rejectedFunction(y) {
@@ -79,15 +165,15 @@ function rejectedFunction(y) {
 }
 
 function shuffleArray(array) {
-    let arr  = []
-    for (let i = 0; i<array.length ; i++) {
-      const j = Math.floor(Math.random() * (i + 1)); // Generate a random index between 0 and i (inclusive)
-      // Swap array[i] and array[j]
+    let arr = []
+    for (let i = 0; i < array.length; i++) {
+        const j = Math.floor(Math.random() * (i + 1)); // Generate a random index between 0 and i (inclusive)
+        // Swap array[i] and array[j]
 
-      let  b = [array[i], array[j]] = [array[j], array[i]];
-      
+        let b = [array[i], array[j]] = [array[j], array[i]];
+
     }
-  }
-  
+}
+
 // ************************* end of fetch section ************************************************************************
 
